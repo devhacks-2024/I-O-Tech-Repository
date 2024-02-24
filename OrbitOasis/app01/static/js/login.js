9dc8ac0cd2877c8f85fc2b67e5b2a98875cfb066
Harry Pu <104880140+HaoRanPu@users.noreply.github.com>
2024-02-24 08:22:34 -0600

    Some change to login,js and login,html

--- OrbitOasis/app01/static/js/login.js
+++ OrbitOasis/app01/static/js/login.js
@@ -7,14 +7,12 @@ document.getElementById("loginForm").addEventListener("submit", (event) => {
     password: document.getElementById("password").value,
   };
 
-  // 更正为 xhrLogin
   xhrLogin.open("POST", "/login", true);
   xhrLogin.setRequestHeader("Content-Type", "application/json");
   xhrLogin.onload = () => {
     // 更正为 xhrLogin.status
     if (xhrLogin.status === 200) {
-      // let xhrDashboard = new XMLHttpRequest();
-      alert("Not implement");
+      window.location.href = "./dashboard.html";
     } else {
       alert("Invalid username or password");
     }
@@ -22,23 +20,4 @@ document.getElementById("loginForm").addEventListener("submit", (event) => {
   xhrLogin.onerror = () =>
     alert("Something gone wrong, Please try again later");
   xhrLogin.send(JSON.stringify(data));
-});
-
-document.getElementById("signUp").addEventListener("click", () => {
-  let xhrLogin = new XMLHttpRequest();
-  xhr.open("GET", "/signup", true);
-  xhr.setRequestHeader("Content-Type", "application/json");
-  xhrLogin.onload = () => {
-    if (xhr.status === 200) {
-      let htmlContent = JSON.parse(xhr.responseText);
-
-      let newWindow = window.open();
-      newWindow.document.open();
-      newWindow.document.write(htmlContent);
-      newWindow.document.close();
-    }
-  };
-  xhrLogin.onerror = () =>
-    alert("Something gone wrong, Please try again later");
-  xhrLogin.send();
 });
