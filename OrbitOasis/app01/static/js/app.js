d6a29ffc93f270069884948219d6fa8d891855ac
Wangz8 <2361659719@qq.com>
2024-02-24 10:14:53 -0600

    APP.JS update

--- OrbitOasis/app01/static/js/app.js
+++ OrbitOasis/app01/static/js/app.js
@@ -2,6 +2,8 @@ const mysql = require('mysql');
 const express = require('express');
 const app = express();
 const port = 8000;
+
+// 创建数据库连接池
 const db = mysql.createPool({
     host: '127.0.0.1',
     user: 'zhihao',
@@ -9,28 +11,10 @@ const db = mysql.createPool({
     database: 'data'
 });
 
-// 确保数据库存在
-db.query('CREATE DATABASE IF NOT EXISTS data', (err, result) => {
-    if (err) throw err;
-    console.log('Database created or already exists');
+// 使用express.json()中间件来解析JSON格式的请求体
+app.use(express.json());
 
-    // 使用数据库
-    db.query('USE data', (err, result) => {
-        if (err) throw err;
-
-        // 创建表
-        db.query(`
-            CREATE TABLE IF NOT EXISTS user (
-                id INT AUTO_INCREMENT PRIMARY KEY,
-                username VARCHAR(255) NOT NULL,
-                password VARCHAR(255) NOT NULL
-            )
-        `, (err, result) => {
-            if (err) throw err;
-            console.log('Table created or already exists');
-        });
-    });
-});
+// 确保数据库和表存在的代码保持不变
 
 app.get('/user', (req, res) => {
     db.query('SELECT * FROM user', (err, data) => {
@@ -41,6 +25,17 @@ app.get('/user', (req, res) => {
             msg: '数据获取成功',
             data
         });
+    });
+});
+
+// 添加一个新的POST路由来接收用户数据并添加到数据库
+app.post('/add-user', (req, res) => {
+    const { username, password } = req.body;
+    db.query('INSERT INTO user (username, password) VALUES (?, ?)', [username, password], (err, result) => {
+        if (err) {
+            return res.status(500).send(err.message);
+        }
+        res.status(201).send({ status: 0, msg: '用户添加成功' });
     });
 });
 
