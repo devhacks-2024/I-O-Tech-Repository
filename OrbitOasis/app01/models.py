eec93581f3efe9ed0506cc16390b9d831eb73fee
Wolfskee <ericshu0223@gmail.com>
2024-08-15 16:57:12 -0500

    update the databse structure, I add the locaton, lat, and lng to table

--- OrbitOasis/app01/models.py
+++ OrbitOasis/app01/models.py
@@ -3,6 +3,9 @@ from django.db import models
 class User(models.Model):
     username = models.CharField(max_length=150, unique=True)
     password = models.CharField(max_length=128)
+    location = models.CharField(max_length=255, null=True, blank=True)
+    lat = models.DecimalField(max_digits=9, decimal_places=6, null=True, blank=True)
+    lng = models.DecimalField(max_digits=9, decimal_places=6, null=True, blank=True)
 
     # 如果你有额外的字段，你可以在这里添加
     # ...
